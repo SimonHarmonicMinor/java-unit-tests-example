bb547a5a530badba181a8d01f5e6ba39dc54bdde
Semyon Kirekov <semyon@kirekov.com>
2023-07-22 12:00:34 +0300

    init

---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+  @Test
+  void testSum() {
+    assertEquals(2, 1 + 1);
+  }
+}
