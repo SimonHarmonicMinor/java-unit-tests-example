0e860cb91c7b09e7e0e08cb693aa673868569eca
Semyon Kirekov <semyon@kirekov.com>
2023-07-23 15:07:51 +0300

    add NotificationActionException

---
+++ src/main/java/org/example/exception/NotificationActionException.java
@@ -0,0 +1,8 @@
+package org.example.exception;
+
+public class NotificationActionException extends RuntimeException {
+
+  public NotificationActionException(String message) {
+    super(message);
+  }
+}
